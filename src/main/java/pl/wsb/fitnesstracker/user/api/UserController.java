a2d856a630594d95dd97270369713f00223bc084
kacper jakubek <kacper.jakubek08@gmail.com>
2025-05-24 09:39:29 +0200

    kjakubek

--- src/main/java/pl/wsb/fitnesstracker/user/api/UserController.java
+++ src/main/java/pl/wsb/fitnesstracker/user/api/UserController.java
@@ -11,6 +11,10 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * REST controller for user operations.
+ * Provides basic CRUD endpoints for users.
+ */
 @RestController
 @RequestMapping("/v1/users")
 @RequiredArgsConstructor
@@ -20,6 +24,11 @@ class UserController {
 
     private final UserMapper userMapper;
 
+    /**
+     * Retrieves all users.
+     *
+     * @return a list of all users as {{@link UserDto}}
+     */
     @GetMapping
     public List<UserDto> getAllUsers() {
         return userService.findAllUsers()
@@ -28,6 +37,11 @@ class UserController {
                 .toList();
     }
 
+    /**
+     * Retrieves basic info of all users.
+     *
+     * @return a list of all users as {{@link SimpleUserDto}}
+     */
     @GetMapping("/simple")
     public List<SimpleUserDto> getAllSimpleUsers() {
         return userService.findAllUsers()
@@ -36,6 +50,11 @@ class UserController {
                 .toList();
     }
 
+    /**
+     * Retrieves user with given id.
+     *
+     * @return a given user as {{@link UserDto}}
+     */
     @GetMapping("/{id}")
     public Optional<UserDto> getUserDetailsById(@PathVariable Long id) {
         return userService.getUser(id)
@@ -44,13 +63,20 @@ class UserController {
                 .findFirst();
     }
 
+    /**
+     * Deletes user with given id from database.
+     */
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUserById(@PathVariable Long id) {
         userService.deleteUser(id);
     }
 
-
+    /**
+     * Retrieves users that contain given string in their emails.
+     *
+     * @return a list of users with matching emails as {{@link UserDto}}
+     */
     @GetMapping("/email")
     public List<UserDto> getUserDetailsByEmail(@RequestParam(required = false) String email) {
         return userService.getUsersByEmail(email)
@@ -58,7 +84,11 @@ class UserController {
                 .map(userMapper::toDto)
                 .toList();
     }
-
+    /**
+     * Retrieves users that were born before given date.
+     *
+     * @return a list of all older than given date users as {{@link UserDto}}
+     */
     @GetMapping("/older/{time}")
     public List<UserDto> getUsersOlderThan(@PathVariable LocalDate time) {
         return userService.getUsersOlderThan(time)
@@ -67,12 +97,24 @@ class UserController {
                 .toList();
     }
 
+    /**
+     * Adds a new user based on the provided UserDto data.
+     *
+     * @param userDto the user data transfer object with information to create a new user
+     * @return {{@link User}} entity saved to database
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public User addUser(@RequestBody UserDto userDto) throws InterruptedException {
         return userService.createUser(userMapper.toEntity(userDto));
     }
 
+    /**
+     * Updates user's data based on the provided UserDto data and given id.
+     *
+     * @param userDto the user data transfer object with new informations about user
+     * @return {{@link User}} entity updated in database
+     */
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     public User updateUser(@RequestBody UserDto userDto, @PathVariable Long id) throws InterruptedException {
